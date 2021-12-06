154d493d95e45970cc8391e564b7f614ddd57447
Zeynep <zeynepkaya_26@hotmail.com>
2021-12-06 20:54:57 +0000

    adding new paths

--- express_server.js
+++ express_server.js
@@ -16,4 +16,8 @@ app.listen(PORT, () => {
 
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
-  });
+});
+
+app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>");
+});
