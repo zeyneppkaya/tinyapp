f44bebdccd41e19e6807f6399d54767056ea88a2
Zeynep <zeynepkaya_26@hotmail.com>
2021-12-08 23:27:32 +0000

    Registering new users and Registration errors

--- express_server.js
+++ express_server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
 app.set("view engine", "ejs");
 
-
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
@@ -13,6 +12,19 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 
 function generateRandomString(num) {
   let randomStr = '';
@@ -41,16 +53,24 @@ app.get("/hello", (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { username: req.cookies['username'], urls: urlDatabase };
+  let templateVars = {
+    user: users[req.cookies["user_id"]],
+    urls: urlDatabase 
+  };
   res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { user: users[req.cookies["user_id"]] };
+  res.render("urls_new", templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { username: req.cookies['username'], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { 
+    user: users[req.cookies["user_id"]],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL] 
+  };
   res.render('urls_show', templateVars);
 });
 
@@ -84,6 +104,33 @@ app.post('/login', (req, res) => {
 });
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls/');
+});
+
+app.get("/register", (req,res) => {
+  let templateVars = { user: users[req.cookies["user_id"]] };
+  res.render("urls_register", templateVars);
+});
+
+app.post("/register", (req,res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const userId = generateRandomString(7);
+  for (let element in users) {
+    if (users[element].email === email) {
+        return res.status(403).send('This email has already been registered.');
+    } else if (!email || !password) {
+        return res.status(403).send('Email and/or password is missing.');
+    }
+  };
+  const newUser = {
+    id: userId,
+    email,
+    password,
+  };
+  users[userId] = newUser;
+  res.cookie('user_id', userId)
+  res.redirect("/urls");
+  console.log(users);
 });
